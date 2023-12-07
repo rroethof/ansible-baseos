02f8956e666ce9020a4088b2e3a061aef8593e7e
Ronny Roethof <ronny.roethof@anylinq.com>
2023-12-07 16:33:08 +0100

    modified molecule

--- molecule.sh
+++ molecule.sh
@@ -8,8 +8,5 @@ pip3 install ansible molecule[docker] ansible-lint yamllint docker
 
 cd molecule
 
-# Run default test (Rocky Linux).
-molecule test
-
 # Run tests on Debian.
 MOLECULE_DISTRO=debian12 molecule test
